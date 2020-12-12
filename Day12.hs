b82cafc0daf936a54c4a89df2106be8175c15800
Tejo Velagapudi <tvelagapudi@imsa.edu>
2020-12-12 15:46:53 -0600

    A little fix to make it more generalized

--- Day12.hs
+++ Day12.hs
@@ -1,6 +1,6 @@
 type Direction    = (Char, Double)
 type Position     = (Double, Double, Double)
-type WayPosition  = (Double, Double, Double, Double)
+type WayPosition  = (Position, Position)
 
 parse :: String -> Direction
 parse (c:cs) = (c, read cs)
@@ -16,19 +16,19 @@ doDirection (x,y,d) dir = case dir of
     ('F', a) -> (x + a * cos (d * pi/180) , y + a * sin (d * pi/180) , d) where
 
 doWayDirection :: WayPosition -> Direction -> WayPosition
-doWayDirection (x,y,w,p) dir = case dir of
-    ('N', a) -> (x, y, w, p + a)
-    ('S', a) -> (x, y, w, p - a)
-    ('E', a) -> (x, y, w + a, p)
-    ('W', a) -> (x, y, w - a, p)
-    ('L', a) -> (x, y, w * cos (a * pi/180) - p * sin (a * pi/180), 
-        p * cos (a * pi/180) + w * sin (a * pi/180))
-    ('R', a) -> (x, y, w * cos (-a * pi/180) - p * sin (-a * pi/180), 
-        p * cos (-a * pi/180) + w * sin (-a * pi/180))
-    ('F', a) -> (x + a * w, y + a * p, w, p)
+doWayDirection ((x,y,d),(w,p,b)) dir = case dir of
+    ('N', a) -> ((x, y, d), (w, p + a, b))
+    ('S', a) -> ((x, y, d), (w, p - a, b))
+    ('E', a) -> ((x, y, d), (w + a, p, b))
+    ('W', a) -> ((x, y, d), (w - a, p, b))
+    ('L', a) -> ((x, y, d), (w * cos (a * pi/180) - p * sin (a * pi/180), 
+        p * cos (a * pi/180) + w * sin (a * pi/180), b))
+    ('R', a) -> ((x, y, d), (w * cos (-a * pi/180) - p * sin (-a * pi/180), 
+        p * cos (-a * pi/180) + w * sin (-a * pi/180), b))
+    ('F', a) -> ((x + a * w, y + a * p, d), (w, p, b))
 
 main :: IO ()
 main = do
     input <- map parse . lines <$> readFile "Input/Day12Input.txt"
-    print . round . (\(x,y,_)   -> abs x + abs y) . foldl doDirection (0,0,0) $ input
-    print . round . (\(x,y,_,_) -> abs x + abs y) . foldl doWayDirection (0,0,10,1) $ input
+    print . round . (\(x,y,_)     -> abs x + abs y) . foldl doDirection (0,0,0) $ input
+    print . round . (\((x,y,_),_) -> abs x + abs y) . foldl doWayDirection ((0,0,0),(10,1,0)) $ input
